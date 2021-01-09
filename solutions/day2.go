024aa82710623886bc8a41ab1176ea7fcb213ae5
cemg <gurescicem@gmail.com>
2021-01-10 00:48:53 +0300

    update day2

--- solutions/day2.go
+++ solutions/day2.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-
-	m := make(map[string]bool)
 
 	c, err := ioutil.ReadFile("../inputs/day2.txt")
 	if err != nil {
@@ -18,9 +17,9 @@ func main() {
 	}
 
 	lines := strings.Split(string(c), "\n")
-	for i, v := range lines {
-		for ii, vv := range lines {
-			for iii, vvv := range lines {
+	for _, v := range lines {
+		for _, vv := range lines {
+			for _, vvv := range lines {
 				iv, err := strconv.Atoi(v)
 				if err != nil {
 					log.Fatal(err)
@@ -33,23 +32,13 @@ func main() {
 				if err != nil {
 					log.Fatal(err)
 				}
-				k1 := fmt.Sprintf("%s+%s+%s", v, vv, vvv)
-				k2 := fmt.Sprintf("%s+%s+%s", v, vvv, vv)
-				k3 := fmt.Sprintf("%s+%s+%s", vv, v, vvv)
-				k4 := fmt.Sprintf("%s+%s+%s", vv, vvv, v)
-				k5 := fmt.Sprintf("%s+%s+%s", vvv, vv, v)
-				k6 := fmt.Sprintf("%s+%s+%s", vvv, v, vv)
-
-				if i == ii || i == iii || m[k1] == true || m[k2] == true || m[k3] == true || m[k4] == true || m[k5] == true || m[k6] == true {
-					continue
+				if iv+ivv+ivvv == 2020 {
+					fmt.Printf("%d * %d * %d = %d \n", iv, ivv, ivvv, iv*ivv*ivvv)
+					os.Exit(0)
 				} else {
-					if iv+ivv+ivvv == 2020 {
-						fmt.Printf("%d * %d * %d = %d \n", iv, ivv, ivvv, iv*ivv*ivvv)
-						break
-					} else {
-						m[k1], m[k2], m[k3], m[k4], m[k5], m[k6] = true, true, true, true, true, true
-					}
+					continue
 				}
+
 			}
 		}
 	}
